bcadc9bdb6fcf6bc0b098dba198911a694dd8894
danisnake1000 <adriandanielfernandez@gmail.com>
2022-05-26 10:08:02 -0300

    hola

---
+++ app.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+//leo: ");t nombre = prompt("ingrese su nombre: ");
+//let apellido = prompt("ingrese su apellido: ");
+let queso = prompt("Ingrese su producto: ");
+let tomate = prompt("ingrese su producto: ");
+
+    //console.log(`Hola ${ nombre} !!!`);
+    //console.log(`Hola ${ apellido} !!!`);
+    console.log(`el  ${ queso} sale $ 100`);
+    console.log(`el  ${ tomate} sale $ 240`);
